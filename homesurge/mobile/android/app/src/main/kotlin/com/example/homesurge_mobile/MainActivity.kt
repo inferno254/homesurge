074a0c81442d93d22fc3f03ab9a854783b3df12a
inferno254 <inferno254@users.noreply.github.com>
2026-07-04 02:20:24 +0300

    Remove mobile from git, add to gitignore

--- homesurge/mobile/android/app/src/main/kotlin/com/example/homesurge_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.homesurge_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- homesurge/mobile/lib/url_launcher_util.dart
+++
@@ -1,9 +0,0 @@
-import 'package:url_launcher/url_launcher.dart';
-
-Future<void> launchExternalUrl(Uri uri) async {
-  final ok = await launchUrl(uri, mode: LaunchMode.externalApplication);
-  if (!ok) {
-    throw Exception('Could not launch $uri');
-  }
-}
-
